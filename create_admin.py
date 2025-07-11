0990263247ac54fa39e84cfc40f11183447ea35d
gs-digital-solution <titus.asaph18@gmail.com>
2025-07-11 03:22:05 +0200

    modifications du 11 juillet 2025 à 3h21

---
+++ create_admin.py
@@ -0,0 +1,23 @@
+﻿import os
+import django
+
+# Configuration de l'environnement Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intelligence.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+def create_admin():
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='titus',
+            email='titus.asaph18@gmail.com',
+            password='intelligence18'  # À modifier impérativement
+        )
+        print("Superuser créé avec succès !")
+    else:
+        print("Un superuser existe déjà")
+
+if __name__ == '__main__':
+    create_admin()
